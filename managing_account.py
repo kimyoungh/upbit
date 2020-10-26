bfdc3cc30fdaa53527254520f813452239694d5f
kimyoungh <eluster107@gmail.com>
2020-10-26 23:25:15 +0900

    init upbit codes

---
+++ managing_account.py
@@ -0,0 +1,221 @@
+"""
+    업비트 계좌 관리 관련 모듈
+
+    @author: Younghyun Kim
+    Created on 2020.10.25
+"""
+import os
+import uuid
+import hashlib
+import jwt
+from urllib.parse import urlencode
+
+import requests
+
+
+class AccountManager:
+    """ Managing Account """
+    def __init__(self):
+        self.access_key = os.environ['UPBIT_OPEN_API_ACCESS_KEY']
+        self.secret_key = os.environ['UPBIT_OPEN_API_SECRET_KEY']
+        self.server_url = os.environ['UPBIT_OPEN_API_SERVER_URL']
+
+    def checking_account(self):
+        " 전체 계좌 조회 "
+        payload = {
+            'access_key': self.access_key,
+            'nonce': str(uuid.uuid4()),
+            }
+
+        jwt_token = jwt.encode(payload, self.secret_key).decode('utf-8')
+        authorize_token = 'Bearer {}'.format(jwt_token)
+        headers = {'Authorization': authorize_token}
+
+        res = requests.get(self.server_url + "/v1/accounts", headers=headers)
+
+        return res.json()
+
+    def get_order_available_info(self, market='KRW-BTC'):
+        """
+            주문 가능 정보 조회
+
+            Args:
+                market: 조회 시장(default: KRW-BTC)
+
+            Return:
+                res: json
+        """
+        query = {
+            'market': market
+        }
+        query_string = urlencode(query).encode()
+
+        m = hashlib.sha512()
+        m.update(query_string)
+        query_hash = m.hexdigest()
+
+        payload = self._get_payload(query_hash, 'SHA512')
+
+        jwt_token = jwt.encode(payload, self.secret_key).decode('utf-8')
+        authorize_token = 'Bearer {}'.format(jwt_token)
+        headers = {"Authorization": authorize_token}
+
+        res = requests.get(self.server_url + '/v1/orders/chance',
+                           params=query, headers=headers)
+
+        return res.json()
+
+    def get_each_order_info(self, order_uuid):
+        """
+            개별 주문 조회: order_uuid를 통해 개별 주문건을 조회한다.
+
+            Args:
+                order_uuid: 주문 uuid
+            Return:
+                res: json
+        """
+        query = {
+            'uuid': order_uuid,
+        }
+        query_string = urlencode(query).encode()
+
+        m = hashlib.sha512()
+        m.update(query_string)
+        query_hash = m.hexdigest()
+
+        payload = self._get_payload(query_hash, 'SHA512')
+
+        jwt_token = jwt.encode(payload, self.secret_key).decode('utf-8')
+        authorize_token = 'Bearer {}'.format(jwt_token)
+        headers = {"Authorization": authorize_token}
+
+        res = requests.get(self.server_url + "/v1/order", params=query,
+                           headers=headers)
+
+        return res.json()
+
+    def get_order_list(self, uuids, state,
+                       market='KRW-BTC', kind='normal',
+                       page=1, limit=100, order_by='desc'):
+        """
+            주문 리스트 조회
+            Args:
+                uuids: list. 주문 고유 아이디 목록
+                state: 주문 상태
+                        - wait: 체결 대기(default)
+                        - done: 전체 체결 완료
+                        - cancel: 주문 취소
+                market: market(default: KRW-BTC)
+                kind: 주문 유형
+                        - normal: 일반 주문
+                        - watch: 예약 주문
+                page: 페이지 수
+                limit: 요청 개수
+                order_by: 정렬 방식
+                            - asc: 오름차순
+                            - desc: 내림차순
+            Return:
+                res: json
+        """
+        query = {
+            'state': state,
+        }
+        query_string = urlencode(query)
+
+        uuids_query_string = '&'.join(["uuids[]={}".format(uid) for uid in uuids])
+
+        query['uuids[]'] = uuids
+        query_string = "{0}&{1}".format(query_string, uuids_query_string).encode()
+
+        m = hashlib.sha512()
+        m.update(query_string)
+        query_hash = m.hexdigest()
+
+        payload = self._get_payload(query_hash, 'SHA512')
+
+        jwt_token = jwt.encode(payload, self.secret_key).decode('utf-8')
+        authorize_token = 'Bearer {}'.format(jwt_token)
+        headers = {'Authorization': authorize_token}
+
+        res = requests.get(self.server_url + "/v1/orders", params=query,
+                           headers=headers)
+
+        return res.json()
+
+    def cancel_order(self, order_uuid):
+        """
+            주문 uuid를 통해 해당 주문에 대한 취소 접수를 한다.
+
+            Args:
+                order_uuid: urder uuid
+            Return:
+                res: json
+        """
+        query = {
+            'uuid': order_uuid,
+        }
+        query_string = urlencode(query).encode()
+
+        m = hashlib.sha512()
+        m.update(query_string)
+        query_hash = m.hexdigest()
+
+        payload = self._get_payload(query_hash, 'SHA512')
+
+        jwt_token = jwt.encode(payload, self.secret_key).decode('utf-8')
+        authorize_token = 'Bearer {}'.format(jwt_token)
+        headers = {'Authorization': authorize_token}
+
+        res = requests.delete(self.server_url + "/v1/order", params=query,
+                              headers=headers)
+
+        return res.json()
+
+    def order(self, side, volume, price, ord_type, market='KRW-BTC'):
+        """ 주문 요청을 한다.
+
+            Args:
+                side: str. 주문 종류(필수)
+                            - bid: 매수
+                            - ask: 매도
+                volume: float. 주문량(지정가, 시장가 매도 시 필수)
+                price: float. 주문 가격(지정가, 시장가 매수 시 필수)
+                ord_type: str. 주문 타입(필수)
+                                - limit: 지정가 주문
+                                - price: 시장가 주문(매수)
+                                - market: 시장가 주문(매도)
+                market: market(default: KRW-BTC)
+        """
+        query = {
+            'market': market,
+            'side': side,
+            'volume': str(volume),
+            'price': str(price),
+            'ord_type': ord_type,
+        }
+        query_string = urlencode(query).encode()
+
+        m = hashlib.sha512()
+        m.update(query_string)
+        query_hash = m.hexdigest()
+
+        payload = self._get_payload(query_hash, 'SHA512')
+
+        jwt_token = jwt.encode(payload, self.secret_key).decode('utf-8')
+        authorize_token = 'Bearer {}'.format(jwt_token)
+        headers = {'Authorization': authorize_token}
+
+        res = requests.post(self.server_url + "/v1/orders", params=query,
+                            headers=headers)
+
+        return res.json()
+
+    def _get_payload(self, query_hash, query_hash_alg='SHA512'):
+        payload = {
+            'access_key': self.access_key,
+            'nonce': str(uuid.uuid4()),
+            'query_hash': query_hash,
+            'query_hash_alg': query_hash_alg,
+        }
+
+        return payload
---
+++ quotation.py
@@ -0,0 +1,138 @@
+"""
+    업비트 시세 조회 모듈
+
+    @author: Younghyun Kim
+    Created on: 2020.10.25
+"""
+import datetime
+import requests
+import pandas as pd
+
+
+class Quotation:
+    " Quotation Class "
+    def __init__(self):
+        self.url = "https://api.upbit.com"
+
+    def get_market_code(self, details=False):
+        """
+            마켓 코드 조회
+
+            Args:
+                details: 유의종목 필드와 같은 상세 정보 노출(default: False)
+
+            Return:
+                res: json
+                     - market: 업비트에서 제공중인 시장 정보
+                     - korean_name: 거래 대상 암호화폐 한글명
+                     - english_name: 거래 대상 암호화폐 영문명
+                     - market_warning: 유의 종목 여부
+                                        - NONE(해당 사항 없음),
+                                        - CAUTION(투자유의)
+        """
+        url = self.url + "/v1/market/all"
+
+        querystring = {"isDetails": str(details).lower()}
+
+        res = requests.request("GET", url, params=querystring)
+
+        return res.text
+
+    def get_market_price(self, period, to=None, unit=1,
+                         market='KRW-BTC', count=1):
+        """
+            시세 조회 메소드
+
+            Args:
+                period: str. 시간단위(minutes, days, weeks, months)
+                to: str. 마지막 캔들 UTC 시각 ('yyyy-MM-dd HH:mm:ss')
+                unit: int. period가 minute일때, 시간 단위(1: 1분)
+                market: market
+                count: int. 최대 조회 가격 수(최대 200)
+            Return:
+                returns: pd.DataFrame
+        """
+        url = self.url + "/v1/candles/" + period
+
+        if period == 'minutes':
+            url += "/" + str(unit)
+
+        querystring = {
+                        'market': market,
+                        'count': str(count),
+        }
+
+        if to is not None:
+            querystring['to'] = to
+
+        response = requests.request('GET', url, params=querystring)
+
+        returns = pd.DataFrame(response.json()).iloc[::-1]
+        times = returns['candle_date_time_kst']
+        times = times.apply(
+            lambda x: datetime.datetime.strptime(x, '%Y-%m-%dT%H:%M:%S'))
+        dates = times.apply(lambda x: x.date())
+        hour = times.apply(lambda x: x.hour)
+        minute = times.apply(lambda x: x.minute)
+        second = times.apply(lambda x: x.second)
+
+        returns['trade_date'] = dates
+        returns['trade_hour'] = hour
+        returns['trade_minute'] = minute
+        returns['trade_second'] = second
+
+        return returns
+
+    def get_recent_traded_data(self, to=None, market='KRW-BTC', count=1, daysAgo=1):
+        """
+            최근 체결 내역
+
+            Args:
+                to: str. HHmmss or HH:mm:ss 마지막 체결 시각
+        """
+        url = self.url + "/v1/trades/ticks"
+
+        querystring = {'count': str(count),
+                       'market': market,
+                       'daysAgo': str(daysAgo)}
+
+        if to is not None:
+            querystring['to'] = to
+
+        res = requests.request("GET", url, params=querystring)
+
+        return res.json()
+
+    def get_recent_price(self, market='KRW-BTC'):
+        """
+            현재가 정보
+        """
+        url = self.url + "/v1/ticker"
+        query = {'market': market}
+
+        res = requests.request('GET', url, params=query)
+
+        return res.json()
+
+    def get_orderbook(self, markets='KRW-BTC'):
+        """
+             호가 정보 조회
+        """
+        url = self.url + "/v1/orderbook"
+
+        query = {'markets': markets}
+
+        res = requests.request('GET', url, params=query)
+
+        return res.json()
+
+    def _convert_kst_to_utc(self, date):
+        """
+            kst 시간을 utc로 변환하기
+        """
+        if isinstance(date, str):
+            date = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+
+        date += datetime.timedelta(hours=-9)
+
+        return date
